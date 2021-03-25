fd4e99a37e0b7deb71628dc9fd65e2e89998404c
Akshay Kumar <reachakshay16@gmail.com>
2021-03-25 21:27:36 +0530

    Implemented the suggestions

--- ASSIGNMENT.sql
+++ ASSIGNMENT.sql
@@ -1,129 +1,210 @@
---CREATE TABLE Actors( Id INT PRIMARY KEY IDENTITY(1,1), 
---Name NVARCHAR(30), 
---Gender NVARCHAR(10),
---DOB DATE)
-
---CREATE TABLE Producers( Id INT PRIMARY KEY IDENTITY(1,1),
---Name NVARCHAR(30),
---Company NVARCHAR(30),
---CompanyEstDate DATE,
---CreatedAt  DATETIME DEFAULT CURRENT_TIMESTAMP)
-
---CREATE TABLE Movies(Id INT PRIMARY KEY IDENTITY(1,1),
---Name NVARCHAR(30),
---Language NVARCHAR(10),
---ProducerId INT FOREIGN KEY REFERENCES Producers(Id),
---Profit INT,
---CreatedAt  DATETIME DEFAULT CURRENT_TIMESTAMP)
-
---CREATE TABLE ActorMovieMapping( Id INT PRIMARY KEY IDENTITY(1,1),
---MovieId INT FOREIGN KEY REFERENCES Movies(Id),
---ActorId INT FOREIGN KEY REFERENCES Actors(Id),
---CreatedAt  DATETIME DEFAULT CURRENT_TIMESTAMP)
-
---INSERT INTO Actors(Name, Gender, DOB) VALUES('
---Robert DeNiro','Male','07/10/1957')
---INSERT INTO Actors(Name, Gender, DOB) VALUES(
-
---'George Michael',
---'Male',
---'11/23/1978'
-
---)
---INSERT INTO Actors(Name, Gender, DOB) VALUES('Mike Scott','Male','08/06/1969')
---INSERT INTO Actors(Name, Gender, DOB) VALUES('Pam Halpert','Female','09/26/1996')
---INSERT INTO Actors(Name, Gender, DOB) VALUES('Dame Judi Dench','Female','04/05/1947')
+CREATE TABLE Actors( 
+Id INT PRIMARY KEY IDENTITY(1,1), 
+Name NVARCHAR(100), 
+Gender NVARCHAR(10),
+DOB DATE,
+CreatedAt  DATETIME DEFAULT CURRENT_TIMESTAMP,
+UpdatedAt  DATETIME DEFAULT CURRENT_TIMESTAMP)
 
 
---SELECT * FROM Actors
+CREATE TABLE Producers( 
+Id INT PRIMARY KEY IDENTITY(1,1),
+Name NVARCHAR(100),
+Company NVARCHAR(30),
+CompanyEstDate DATE,
+CreatedAt  DATETIME DEFAULT CURRENT_TIMESTAMP,
+UpdatedAt  DATETIME DEFAULT CURRENT_TIMESTAMP)
 
---INSERT INTO Producers(Name, Company,CompanyEstDate) VALUES('Arjun','Fox','05/14/1998')
---INSERT INTO Producers(Name, Company,CompanyEstDate) VALUES('Arun','Bull','09/11/2004')
---INSERT INTO Producers(Name, Company,CompanyEstDate) VALUES('Tom','Hanks','11/03/1987')
---INSERT INTO Producers(Name, Company,CompanyEstDate) VALUES('Zeshan','Male','11/14/1996')
---INSERT INTO Producers(Name, Company,CompanyEstDate) VALUES('Nicole','Team Coco','09/26/1992')
+CREATE TABLE Movies(
+Id INT PRIMARY KEY IDENTITY(1,1),
+Name NVARCHAR(100),
+Language NVARCHAR(10),
+ProducerId INT FOREIGN KEY REFERENCES Producers(Id),
+Profit INT,
+CreatedAt  DATETIME DEFAULT CURRENT_TIMESTAMP,
+UpdatedAt  DATETIME DEFAULT CURRENT_TIMESTAMP)
+
+CREATE TABLE ActorMovieMapping( 
+Id INT PRIMARY KEY IDENTITY(1,1),
+MovieId INT FOREIGN KEY REFERENCES Movies(Id),
+ActorId INT FOREIGN KEY REFERENCES Actors(Id),
+CreatedAt  DATETIME DEFAULT CURRENT_TIMESTAMP,
+UpdatedAt  DATETIME DEFAULT CURRENT_TIMESTAMP)
+GO
+
+CREATE TRIGGER Actor_update on Actors FOR UPDATE AS
+BEGIN
+  UPDATE Actors
+	SET UpdatedAt = GETDATE()
+	FROM Actors INNER JOIN Deleted D
+	on Actors.Id=D.Id
+END
+GO
+
+CREATE TRIGGER Producer_update on Producers FOR UPDATE AS
+BEGIN
+  UPDATE Producers
+	SET UpdatedAt = GETDATE()
+	FROM Producers INNER JOIN Deleted D
+	on Producers.Id=D.Id
+END
+GO
+
+CREATE TRIGGER Movie_update on Movies FOR UPDATE AS
+BEGIN
+  UPDATE Movies
+	SET UpdatedAt = GETDATE()
+	FROM Movies INNER JOIN Deleted D
+	on Movies.Id=D.Id
+END
+GO 
+
+CREATE TRIGGER ActorMovieMapping_update on ActorMovieMapping FOR UPDATE AS
+BEGIN
+  UPDATE ActorMovieMapping
+	SET UpdatedAt = GETDATE()
+	FROM ActorMovieMapping INNER JOIN Deleted D
+	on ActorMovieMapping.Id=D.Id
+END
+GO
+
+INSERT INTO Actors(Name, Gender, DOB) 
+VALUES
+('Mila Kunis','Female','11/14/1986'),
+('Robert DeNiro','Male','07/10/1957'),
+('George Michael','Male','11/23/1978'),
+('Mike Scott','Male','08/06/1969'),
+('Pam Halpert','Female','09/26/1996'),
+('Dame Judi Dench','Female','04/05/1947')
+
+INSERT INTO Producers(Name, Company,CompanyEstDate) 
+VALUES
+('Arjun','Fox','05/14/1998'),
+('Arun','Bull','09/11/2004'),
+('Tom','Hanks','11/03/1987'),
+('Zeshan','Male','11/14/1996'),
+('Nicole','Team Coco','09/26/1992'),
+('test','test','1992')
+
+INSERT INTO Movies(Name, Language, ProducerId, Profit) 
+VALUES
+('Rocky','English',1,10000),
+('Rocky','Hindi',3,3000),
+('Terminal','English',4,300000),
+('Rambo','Hindi',2,93000),
+('Rudy','English',5,9600)
+
+INSERT INTO ActorMovieMapping(MovieId,ActorId) 
+VALUES
+(1,1),(1,3),(1,5),
+(2,6),(2,5),(2,4),(2,2),
+(3,3),(3,2),
+(4,1),(4,6),(4,3),
+(5,2),(5,5),(5,3)
 
 
---INSERT INTO Movies(Name, Language, ProducerId, Profit) VALUES('Rocky','English',1,10000)
---INSERT INTO Movies(Name, Language, ProducerId, Profit) VALUES('Rocky','Hindi',3,3000)
---INSERT INTO Movies(Name, Language, ProducerId, Profit) VALUES('Terminal','English',4,300000)
---INSERT INTO Movies(Name, Language, ProducerId, Profit) VALUES('Rambo','Hindi',2,93000)
---INSERT INTO Movies(Name, Language, ProducerId, Profit) VALUES('Rudy','English',5,9600)
+--Update Profit of all the movies by +1000 where producer name contains 'run'
+UPDATE Movies 
+SET Profit = Profit + 1000 
+FROM Movies M INNER JOIN Producers P 
+ON M.ProducerId = P.Id 
+WHERE P.Name LIKE '%run%'
 
+--Find duplicate movies having the same name and their count
+SELECT Name AS MovieName, COUNT(*) AS MovieCount 
+FROM Movies 
+GROUP	BY Name 
+HAVING COUNT(*) > 1
 
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(1,1)
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(1,3)
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(1,5)
+--Find the oldest actor/actress for each movie
+SELECT R.Name,  A.Name 
+FROM 
+(
+SELECT M.Id,M.Name, MIN(A.DOB) AS MinimumDOB
+FROM Actors A INNER JOIN ActorMovieMapping AM 
+ON A.Id = AM.ActorId
+INNER JOIN Movies M 
+ON AM.MovieId = M.Id
+GROUP BY M.Id,M.Name
+)R 
+INNER JOIN ActorMovieMapping AM 
+ON AM.MovieId = R.Id INNER JOIN Actors A 
+ON A.Id = AM.ActorId
+WHERE A.DOB = MinimumDOB
 
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(2,6)
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(2,5)
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(2,4)
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(2,2)
+--List of producers who have not worked with actor 'Mila Kunis'
+--Approach using IN 
+SELECT p.Name 
+FROM Producers p 
+WHERE p.Id NOT IN
+(
+SELECT p.Id
+FROM Producers p,
+	(SELECT m.Name 'Movie Name',a.Name 'Actor Name',m.ProducerId,am.ActorId
+	FROM Movies m , ActorMovieMapping am, Actors a
+	WHERE m.id =  am.MovieId
+	AND am.ActorId = a.Id
+	AND a.Name = 'mila kunis'
+	) r 
+WHERE P.Id = r.ProducerId)
 
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(3,3)
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(3,2)
+--		( OR )
 
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(4,1)
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(4,6)
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(4,3)
+-- Approach using left join
+SELECT P.* , R.*
+FROM Producers P LEFT JOIN 
+	(
+	SELECT M.ProducerId, AM.ActorId
+	FROM  Movies M 
+	INNER JOIN ActorMovieMapping AM 
+	ON M.Id = AM.MovieId
+	INNER JOIN Actors A 
+	ON AM.ActorId = a.Id
+	WHERE A.Name = 'Mila Kunis'
+	) R 
+ON R.ProducerId = P.Id
+WHERE R.ProducerId IS NULL
 
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(5,2)
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(5,5)
---INSERT INTO ActorMovieMapping(MovieId,ActorId) VALUES(5,3)
+--List of pair of actors who have worked together in more than 2 movies
+SELECT A1.name, A2.name, CNT 
+FROM Actors A1
+INNER JOIN 
+	(
+	SELECT AM1.ActorId,AM2.ActorId as Actorid2, COUNT(*) CNT
+	FROM ActorMovieMapping AM1 JOIN ActorMovieMapping AM2 
+	ON (AM1.ActorId > AM2.ActorId AND AM1.MovieId = AM2.MovieId)
+	GROUP BY AM1.ActorId,AM2.ActorId
+	HAVING COUNT(*) >= 2
+	) R 
+ON A1.Id = R.ActorId
+INNER JOIN  Actors A2 ON R.Actorid2 = A2.Id
 
-
-
-
---UPDATE Movies SET Profit = Profit + 1000 
---FROM Movies M INNER JOIN Producers P ON M.ProducerId = P.Id 
---WHERE P.Name LIKE '%run%'
-
---SELECT Name AS MovieName, COUNT(*) AS MovieCount 
---FROM Movies 
---GROUP	BY Name 
---HAVING COUNT(*) > 1
-
---SELECT R.Name,  A.Name from (
---SELECT M.Id,M.Name, MIN(A.DOB) AS MinimumDOB
---FROM Actors A INNER JOIN ActorMovieMapping AM ON A.Id = AM.ActorId
---INNER JOIN Movies M ON AM.MovieId = M.Id
---GROUP BY M.Id,M.Name
---)R JOIN ActorMovieMapping AM ON AM.MovieId = R.Id JOIN Actors A ON A.Id = AM.ActorId
---WHERE A.DOB = MinimumDOB
-
---SELECT p.Name FROM
---Producers p WHERE p.Id NOT IN
---(SELECT p.Id
---FROM Producers p,
---(SELECT m.Name 'Movie Name',a.Name 'Actor Name',m.ProducerId,am.ActorId
---FROM Movies m , ActorMovieMapping am, Actors a
---WHERE m.id =  am.MovieId
---AND am.ActorId = a.Id
---AND a.Name = 'mila kunis'
---) r 
---WHERE P.Id = r.ProducerId)
-
-		--( OR )
-
-
---select p.* , r.*from producers p left join (select m.ProducerId, am.ActorId
---from  Movies M 
---inner join ActorMovieMapping am on m.Id = am.MovieId
---inner join Actors a on am.ActorId = a.Id
---where a.name = 'Mila Kunis')r 
---on r.ProducerId = p.Id
---where r.ProducerId IS NULL
-
---SELECT a1.name, a2.name FROM Actors a1
---INNER JOIN 
---(SELECT am1.ActorId,am2.ActorId as actorid2, COUNT(*) cnt
---FROM ActorMovieMapping am1 JOIN ActorMovieMapping am2 ON (am1.ActorId > am2.ActorId AND am1.MovieId = am2.MovieId)
---GROUP BY am1.ActorId,am2.ActorId
---HAVING COUNT(*) >= 2) r ON a1.Id = r.ActorId
---INNER JOIN  Actors a2 ON r.actorid2 = a2.Id
-
---CREATE NONCLUSTERED INDEX movies_profit ON Movies(Profit)
---EXEC SelectAllActors @MovieID = 1
---SELECT dbo.AGE(A.DOB) FROM Actors A;
---EXEC IncreaseProfit @MovieIds = '4,5'
-
+--Add non-clustered index on profit column of movies table
+CREATE NONCLUSTERED INDEX movies_profit ON Movies(Profit)
+GO
+--Create stored procedure to return list of actors for given movie id
+CREATE PROCEDURE SelectAllActors @MovieID INT  
+AS  
+BEGIN
+	SELECT A.Name   
+	FROM Actors A   
+	INNER JOIN ActorMovieMapping AMM  
+	ON AMM.ActorId = A.Id  
+	WHERE AMM.MovieId = @MovieID
+END
+GO
+--Create a function to return age for given date of birth
+CREATE FUNCTION CalculateAge( @DOB DATE)
+RETURNS INT
+AS
+BEGIN
+RETURN ( SELECT DATEDIFF(YEAR, @DOB, CURRENT_TIMESTAMP) )
+END
+GO
+--Create a stored procedure to increase the profit (+100) of movies with given Ids (comma separated) 
+CREATE PROCEDURE IncrementProfit @MovieIds varchar(20)  
+AS  
+UPDATE Movies 
+SET Profit = Profit + 100 
+WHERE Id 
+IN (SELECT * FROM string_split(@MovieIds,','))
--- classses.sql
+++ classses.sql
@@ -1,45 +1,103 @@
-CREATE TABLE Classes(Id INT IDENTITY(1,1) PRIMARY KEY, Name NVARCHAR(20),Section NVARCHAR(10), Number INT)
-CREATE TABLE Teachers(Id INT IDENTITY(1,1) PRIMARY KEY, Name NVARCHAR(20), DOB Date, Gender NVARCHAR(10))
-CREATE TABLE Students(Id INT IDENTITY(1,1) PRIMARY KEY, Name NVARCHAR(20), DOB Date, Gender NVARCHAR(10), ClassId INT FOREIGN KEY REFERENCES Classes(Id))
-CREATE TABLE TeacherClassMapping(TeacherId INT FOREIGN KEY REFERENCES Teachers(Id), ClassId INT FOREIGN KEY REFERENCES Classes(Id));
+CREATE TABLE Classes(
+Id INT IDENTITY(1,1) PRIMARY KEY, 
+Name NVARCHAR(20), 
+Section NVARCHAR(10), 
+Number INT)
 
-INSERT INTO Classes values ('IX','A',201)
-INSERT INTO Classes values ('IX','B',202)
-INSERT INTO Classes values ('X','A',203)
+CREATE TABLE Teachers(
+Id INT IDENTITY(1,1) PRIMARY KEY,
+Name NVARCHAR(20), DOB Date,
+Gender NVARCHAR(10))
 
-INSERT INTO Teachers values ('Lisa Kudrow','1985/06/08','Female')
-INSERT INTO Teachers values ('Monica Bing','1982/03/06','Female')
-INSERT INTO Teachers values ('Chandler Bing','1978/12/17','Male')
-INSERT INTO Teachers values ('Ross Geller','1993/01/26','Male')
+CREATE TABLE Students(
+Id INT IDENTITY(1,1) PRIMARY KEY,
+Name NVARCHAR(20), 
+DOB Date, 
+Gender NVARCHAR(10), 
+ClassId INT FOREIGN KEY REFERENCES Classes(Id))
 
-INSERT INTO Students values ('Scotty Loman','2006/01/31','Male',1)
-INSERT INTO Students values ('Adam Scott','2005/06/01','Male',1)
-INSERT INTO Students values ('Natosha Beckles','2005/01/23','Female',2)
-INSERT INTO Students values ('Lilly Page','2006/11/26','Female',2)
-INSERT INTO Students values ('John Freeman','2006/06/14','Male',2)
-INSERT INTO Students values ('Morgan Scott','2005/05/18','Male',3)
-INSERT INTO Students values ('Codi Gass','2005/12/24','Female',3)
-INSERT INTO Students values ('Nick Roll','2005/12/24','Male',3)
-INSERT INTO Students values ('Dave Grohl','2005/02/12','Male',3)
+CREATE TABLE TeacherClassMapping(
+TeacherId INT FOREIGN KEY REFERENCES Teachers(Id),
+ClassId INT FOREIGN KEY REFERENCES Classes(Id));
 
-INSERT INTO TeacherClassMapping values (1,1)
-INSERT INTO TeacherClassMapping values (1,2)
-INSERT INTO TeacherClassMapping values (2,2)
-INSERT INTO TeacherClassMapping values (2,3)
-INSERT INTO TeacherClassMapping values (3,3)
-INSERT INTO TeacherClassMapping values (3,1)
+INSERT INTO Classes(Name,Section,Number) 
+values 
+('IX','A',201),
+('IX','B',202),
+('X','A',203)
 
+INSERT INTO Teachers(Name,DOB,Gender) 
+values 
+('Lisa Kudrow','1985/06/08','Female'),
+('Monica Bing','1982/03/06','Female'),
+('Chandler Bing','1978/12/17','Male'),
+('Ross Geller','1993/01/26','Male')
 
-SELECT * FROM Students WHERE Gender = 'Male';
-SELECT * FROM Students WHERE DOB >= '2005/01/01';
-SELECT * FROM Students WHERE DOB = (select MAX(DOB) FROM Students);
-SELECT DISTINCT DOB FROM Students;
-SELECT ClassId, COUNT(*) as Count FROM Students GROUP BY ClassId;
-SELECT C.Section, COUNT(*) AS Count from Classes C INNER JOIN Students S ON C.Id = S.ClassId GROUP BY C.Section;
-SELECT  T.Name , COUNT(*) AS Count FROM Teachers T INNER JOIN TeacherClassMapping TCM ON T.Id = TCM.TeacherId GROUP BY T.Name;
-SELECT T.Name , C.Name, C.Section FROM Teachers T INNER JOIN TeacherClassMapping TCM ON T.Id = TCM.TeacherId INNER JOIN Classes C ON C.Id = TCM.ClassId WHERE C.Name = 'ix'
-SELECT ClassId FROM TeacherClassMapping GROUP BY ClassId HAVING COUNT(*) > 2
-SELECT S.* FROM Students S INNER JOIN Classes C ON C.Id = S.ClassId INNER JOIN TeacherClassMapping TCM ON TCM.ClassId = C.Id INNER JOIN Teachers T ON T.Id = TCM.TeacherId WHERE T.Name LIKE '%Lisa%'
+INSERT INTO Students(Name,DOB,Gender,ClassId) 
+values
+('Scotty Loman','2006/01/31','Male',1),
+('Adam Scott','2005/06/01','Male',1),
+('Natosha Beckles','2005/01/23','Female',2),
+('Lilly Page','2006/11/26','Female',2),
+('John Freeman','2006/06/14','Male',2),
+('Morgan Scott','2005/05/18','Male',3),
+('Codi Gass','2005/12/24','Female',3),
+('Nick Roll','2005/12/24','Male',3),
+('Dave Grohl','2005/02/12','Male',3)
+
+INSERT INTO TeacherClassMapping(TeacherId,ClassId) values 
+(1,1),
+(1,2),
+(2,2),
+(2,3),
+(3,3),
+(3,1)
+
+--Find list of male students 
+SELECT * 
+FROM Students 
+WHERE Gender = 'Male';
+
+--Find list of student older than 2005/01/01
+SELECT * 
+FROM Students 
+WHERE DOB >= '2005/01/01';
+
+--Youngest student in school
+SELECT * 
+FROM Students 
+WHERE DOB = (select MAX(DOB) FROM Students);
+
+--Find student distinct birthdays
+SELECT DISTINCT DOB 
+FROM Students;
+
+-- No of students in each class
+SELECT ClassId, COUNT(*) as Count 
+FROM Students 
+GROUP BY ClassId;
+
+-- No of students in each section
+SELECT C.Section, COUNT(*) AS Count 
+FROM Classes C INNER JOIN Students S 
+ON C.Id = S.ClassId 
+GROUP BY C.Section;
+
+-- No of classes taught by teacher
+SELECT  T.Name , COUNT(*) AS Count 
+FROM Teachers T INNER JOIN TeacherClassMapping TCM 
+ON T.Id = TCM.TeacherId 
+GROUP BY T.Name;
+
+-- List of teachers teaching Class X
+SELECT T.Name , C.Name, C.Section 
+FROM Teachers T INNER JOIN TeacherClassMapping TCM 
+ON T.Id = TCM.TeacherId 
+INNER JOIN Classes C 
+ON C.Id = TCM.ClassId 
+WHERE C.Name = 'x'
+
+--Classes which have more than 2 teachers teaching
 SELECT C.name as Class_Name 
 FROM Classes C
 JOIN TeacherClassMapping TCM
@@ -48,4 +106,14 @@ JOIN Teachers T
 ON T.id = TCM.TeacherId
 GROUP BY C.name
 HAVING COUNT(TCM.TeacherId)>2
+
+-- List of students being taught by 'Lisa'
+SELECT S.* 
+FROM Students S INNER JOIN Classes C 
+ON C.Id = S.ClassId INNER JOIN TeacherClassMapping TCM 
+ON TCM.ClassId = C.Id INNER JOIN Teachers T 
+ON T.Id = TCM.TeacherId 
+WHERE T.Name LIKE '%Lisa%'
+
+
 
